29c87fc914f1ee157d876c5f78966b0f5f291794
ChoiHyoHun <hh04460446@gmail.com>
2024-08-10 12:12:51 +0900

    stage2 구조 조금 수정

--- Assets/HyoHun/LockRotation.cs
+++ Assets/HyoHun/LockRotation.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class LockRotation : MonoBehaviour
 {
-    private Vector3 initailPosition;
+    //private Vector3 initailPosition;
     private Quaternion initialRotation;
 
     void Start()
     {
         // 초기 회전 및 위치 저장
-        initailPosition = transform.position;
+        //initailPosition = transform.position;
         initialRotation = transform.rotation;
     }
 
     void LateUpdate()
     {
         // 자식 오브젝트 고정
-        transform.position = initailPosition;
+        //transform.position = initailPosition;
         transform.rotation = initialRotation;
     }
 }
